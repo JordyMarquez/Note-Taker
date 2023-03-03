609ce07190b51ccf1346d304d51516ecd2c8f947
Jordy Marquez <jordymarquez@MacBook-Pro.local>
2023-03-02 17:16:32 -0800

    added routes folder and crated server.js file w/code inside

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,22 @@
+// imports required packages to run app
 const path = require('path')
 const express = require('express')
+// uses npm pacage of uuuid to give each notea unique id 
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 
 const api = express();
 
+// connects path to db.json
 api.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
 });
 
-
+// post method reads db.json file and returns saved ntoes
 api.post('/notes', (req, res) => {
+
+    // creates body of a new note
     const { title, text } = req.body;
     if (title && text) {
         const newNote = {
@@ -19,6 +24,7 @@ api.post('/notes', (req, res) => {
             text,
             id: uuidv4(),
         };
+        // reads db.json file
         fs.readFile('db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
@@ -27,6 +33,7 @@ api.post('/notes', (req, res) => {
                 const parsedNotes = JSON.parse(data);
                 parsedNotes.push(newNote);
 
+                // writes to the db.json fille
                 fs.writeFile('db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) =>
                     writeErr
                         ? console.error(writeErr)
@@ -37,11 +44,12 @@ api.post('/notes', (req, res) => {
         const note = {
             body: newNote
         };
-
+        // attaches new note into response
         res.status(201).json(newNote);
     }
 })
 
+// deletes note
 api.delete('/notes/:id', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) {
@@ -61,8 +69,5 @@ api.delete('/notes/:id', (req, res) => {
     }
     })
 });
-// / GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 module.exports = api
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,14 @@
+// imports required packages to run app
 const path = require('path')
 const express = require('express') //what does Router do
 
 const HTML = express();
 
+// connects path to notes.html
 HTML.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
+// connects path to index.html
 HTML.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
